325e7f51ad5792a7cb21afcb550393a8ae811369
Kumar Gaurav <66477464+krgauravsk@users.noreply.github.com>
2023-01-15 03:00:09 +0530

    Initial commit

---
+++ src/components/Form.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+//For Icons-Search-library
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+library.add(faSearch);
+
+const Input = (props) => (
+  <div className="form">
+    <input
+      className="input"
+      placeholder="Type your location..."
+      onChange={props.onChange}
+      required
+    />
+    <button type="submit" className="btn" onClick={props.onClick}>
+      <FontAwesomeIcon
+        className="icon"
+        size="lg"
+        icon={faSearch}
+        fixedWidth
+      ></FontAwesomeIcon>
+    </button>
+  </div>
+);
+
+export default Input;
